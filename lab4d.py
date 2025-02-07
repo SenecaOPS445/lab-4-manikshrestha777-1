7ea1daba942181a17e3b21961932b085b3c93f3c
Manik Shrestha <mshrestha8@myseneca.ca>
2025-02-07 11:48:52 -0500

    final push

---
+++ lab4d.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# Strings 1
+
+str1 = 'Hello World!!'
+str2 = 'Seneca College'
+
+num1 = 1500
+num2 = 1.50
+
+def first_five(input_str):
+    # return the first five characters of input string
+    return input_str[:5]
+
+def last_seven(input_str):
+    # return the last seven characters of input string
+    return input_str[-7:]
+
+def middle_number(number):
+    # convert the integer to string and return the second and third characters
+    num_str = str(number)
+    return num_str[1:3]
+
+def first_three_last_three(str1, str2):
+    # return a string that starts with the first three characters of str1 and ends with the last three charaters of str2 
+    return str1[:3] + str2[-3:]
+
+if __name__ == '__main__':
+    print(first_five(str1))
+    print(first_five(str2))
+    print(last_seven(str1))
+    print(last_seven(str2))
+    print(middle_number(num1))
+    print(middle_number(num2))
+    print(first_three_last_three(str1, str2))
+    print(first_three_last_three(str2, str1))
